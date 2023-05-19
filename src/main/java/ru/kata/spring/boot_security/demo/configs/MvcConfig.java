98d960f8aec12e6d8982d9bea5637e1fd4596926
Hutzy <whodoctor553@gmail.com>
2023-05-19 11:22:27 +0300

    js

--- src/main/java/ru/kata/spring/boot_security/demo/configs/MvcConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/MvcConfig.java
@@ -6,13 +6,12 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableWebMvc
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("redirect:/login");
         registry.addViewController("/user").setViewName("user");
-        registry.addViewController("/admin/**").setViewName("admin");//"redirect: /login"
+        registry.addViewController("/admin").setViewName("admin");//"redirect: /login"
     }
 
     @Override
